02085c82b8def0f2e4258f50e53c5d1ef98a9d3f
FairozAhmadSheikh <fairozahmadsheikh@gmail.com>
2026-05-26 10:51:46 +0530

    Health check route defind

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from fastapi import FastAPI,Path,HTTPException
 import pickle
 from fastapi.responses import JSONResponse
 import pandas as pd
+from schema.User_Input import UserInput
+
+MODEL_VERSION="1.0.0"
 
 app=FastAPI()
 
@@ -10,14 +13,18 @@ app=FastAPI()
 with open('model/Model.pkl','rb')as f:
     model=pickle.load(f)
 
-
-
-
-    
-
+# Home Route 
 @app.get('/')
 def home():
     return JSONResponse(status_code=200,content={"message":"This is a Insurance prediction API 💖"})
+
+# Health check route for Cloud Platform Deployment
+@app.post('/health')
+def health_check():
+    return {"status":'OK',
+            'Model_Version':MODEL_VERSION,
+            'Loaded_Model':model is not None
+            }
 
 @app.post('/predict')
 def predict_premium(data: UserInput):
